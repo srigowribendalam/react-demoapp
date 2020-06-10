4dc3d4a027dde6d998909b24f8bc33a11ad9a204
Srigowri Bendalam <srigowri997@gmail.com>
2020-06-10 22:50:33 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './App.css';
+import Patron from "./Components/patron";
+import Ngo from "./Components/ngo";
+import Event from "./Components/events";
+
+
+function App() {
+    return (
+
+        <div className="App">
+            <Patron />
+            <Ngo />
+            <Event/>
+
+        </div>
+
+    );
+}
+
+export default App;
